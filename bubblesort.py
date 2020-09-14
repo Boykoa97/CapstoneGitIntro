81ba6ff994a056f58b4de303dfefaac0b88294c2
Anthony <anthony.j.boyko@gmail.com>
2020-09-14 10:22:08 -0700

    finished bubblesort

---
+++ bubblesort.py
@@ -0,0 +1,23 @@
+# bubble sort start from index 0 and moves through an array but iteratively
+# swapping elements that are to the right where the right element is smaller than the ith element
+
+
+def bubbleSort(array):
+    # array is the unsorted array
+    temp = 0;  #this is a temporary variable to hold the information of the element being swapped
+
+    #loop through n elements in the array
+    for i in range(len(array)):
+        #loop through each element till index i is in the correct position
+        #we know the the ith elements from the end are in position due to the earlier iterations
+        for j in range(0,len(array)-i-1):
+            if array[j] > array[j+1] : #if element to the right is bigger swap
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+
+
+
+testArray = [5,4,8,2,10,15,22,17]
+bubbleSort(testArray)
+print(testArray)
